568594f905472c0f21c21695b7d5ef0e9a47560e
Thommell <thomslaghekke@outlook.com>
2024-10-02 13:03:10 +0200

    Main Menu Version 1.00v
    Added QuitButton and a PlayButton

--- Monogame_Project1/Engine/Scenes/MainMenu.cs
+++ Monogame_Project1/Engine/Scenes/MainMenu.cs
@@ -1,14 +1,21 @@
 using Monogame_Project1.Engine.BaseClasses;
+using Monogame_Project1.Engine.GameObjects;
 
 namespace Monogame_Project1.Engine.Scenes;
 
 public class MainMenu : Scene
 {
-    public MainMenu(Game1 pGame, SceneManager pManager) : base(pGame, pManager)
-    { }
-
+    private QuitButton _quitButton;
+    public MainMenu(Game1 pGame, SceneManager pManager) : base(pGame, pManager) { }
     public override void LoadContent(ContentManager pContent)
     {
-        objects.Add(new Button(game, manager, game.Content.Load<Texture2D>("UI_Tile_128x64"), "Quit"));
+        objects.Add(_quitButton = new(game, manager, game.Content.Load<Texture2D>("UI_Tile_128x64"), "Quit")
+        {
+          Position = new Vector2(game.GraphicsDevice.Viewport.Width * 0.5f, game.GraphicsDevice.Viewport.Height * 0.5f + 64)
+        });
+        objects.Add(new PlayButton(game, manager, game.Content.Load<Texture2D>("UI_Tile_128x64"), "Play")
+        {
+            Position = new Vector2(_quitButton.Position.X, _quitButton.Position.Y - _quitButton.Texture.Height * 1.5f)
+        });
     }
 }
