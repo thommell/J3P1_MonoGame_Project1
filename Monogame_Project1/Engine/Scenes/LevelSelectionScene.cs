58b26a2a191c687cb708cfc66881ceb2b92e541c
Jannis Tev <jteveluwe01@student.rocvantwente.nl>
2024-10-02 15:31:06 +0200

    feat: LevelSelectionScene
    Unfinished

---
+++ Monogame_Project1/Engine/Scenes/LevelSelectionScene.cs
@@ -0,0 +1,15 @@
+﻿using Monogame_Project1.Engine.BaseClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame_Project1.Engine.Scenes;
+
+public class LevelSelectionScene : Scene
+{
+    public LevelSelectionScene(Game1 pGame, SceneManager pManager) : base(pGame, pManager)
+    {
+    }
+}
