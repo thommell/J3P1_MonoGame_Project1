5c76f9337bdb88cea415bf297eb28e60c97cf336
Jannis Tev <jteveluwe01@student.rocvantwente.nl>
2024-10-04 09:45:30 +0200

    feat: level Selection screen
    Added a scene where you can select certain levels.

--- Monogame_Project1/Engine/GameObjects/SelectionScreenButton.cs
+++ Monogame_Project1/Engine/GameObjects/SelectionScreenButton.cs
@@ -1,24 +1,39 @@
 ﻿using Monogame_Project1.Engine.BaseClasses;
-using Monogame_Project1.Engine.Scenes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Monogame_Project1.Engine.GameObjects
 {
     internal class SelectionScreenButton : Button
     {
-        private Scene _sceneToSwitchTo;
-        public SelectionScreenButton(Game1 pGame, SceneManager pManager, Texture2D pTexture, string text, Scene pSceneToSwitchTo) : base(pGame, pManager, pTexture, text) { 
-        
-            _sceneToSwitchTo = pSceneToSwitchTo;
-        }
+        private Texture2D _lock;
 
+        private Scene _sceneToSwitchTo;
+        private bool _locked;
+
+        public SelectionScreenButton(Game1 pGame, SceneManager pManager, Texture2D pTexture, string text, Scene pSceneToSwitchTo, Texture2D pLock, bool pLocked = false) : base(pGame, pManager, pTexture, text)
+        {
+            _sceneToSwitchTo = pSceneToSwitchTo;
+            _locked = pLocked;
+            _lock = pLock;
+        }
+        public override void Update(GameTime pGameTime)
+        {
+            if (_locked) return;
+
+            base.Update(pGameTime);
+        }
+        public override void Draw(SpriteBatch pSpriteBatch)
+        {
+            base.Draw(pSpriteBatch);
+
+            if (_locked) pSpriteBatch.Draw(_lock, new Vector2(position.X - texture.Width / 2, position.Y - texture.Height / 2), Color.White);           
+        }
         protected override void OnClick()
         {
-            manager.ChangeScene(_sceneToSwitchTo);
+            manager.ChangeScene(_sceneToSwitchTo);   
+        }
+        public void Unlock()
+        {
+            _locked = false;
         }
     }
 }
