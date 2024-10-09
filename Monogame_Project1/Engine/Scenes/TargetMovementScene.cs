00963f2c9117b4bc4e4aed20ac1bd16ad17772bc
Thommell <thomslaghekke@outlook.com>
2024-10-09 13:15:13 +0200

    Init TargetMovement

---
+++ Monogame_Project1/Engine/Scenes/TargetMovementScene.cs
@@ -0,0 +1,10 @@
+using Monogame_Project1.Engine.BaseClasses;
+
+namespace Monogame_Project1.Engine.Scenes;
+
+public class TargetMovementScene : Scene
+{
+    public TargetMovementScene(Game1 pGame, SceneManager pManager) : base(pGame, pManager) { }
+    
+    
+}
